30287c2c12283774316bed9879fcd8733e7467eb
calvin1994 <30905708+calvin1994@users.noreply.github.com>
2024-09-22 23:04:33 +0200

    party share exp flag set

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1220,7 +1220,7 @@
 
 #define FLAG_RUNNING_SHOES_TOGGLE                                   0x493
 #define FLAG_PARTY_MOVES                                            0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
+#define FLAG_PARTY_EXP_SHARE                                         0x495 // Unused Flag
 #define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
