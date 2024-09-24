1413fd383d143a873142f1c32824206af61f84c1
calvin1994 <30905708+calvin1994@users.noreply.github.com>
2024-09-24 19:50:49 +0200

    Update flags.h

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1218,9 +1218,9 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
-#define FLAG_RUNNING_SHOES_TOGGLE                                   0x493
-#define FLAG_PARTY_MOVES                                            0x494 // Unused Flag
-#define FLAG_PARTY_EXP_SHARE                                         0x495 // Unused Flag
+#define FLAG_RUNNING_SHOES_TOGGLE                                   0x493 // Custom Flag
+#define FLAG_PARTY_MOVES                                            0x494 // Custom Flag
+#define FLAG_PARTY_EXP_SHARE                                        0x495 // Custom Flag
 #define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
